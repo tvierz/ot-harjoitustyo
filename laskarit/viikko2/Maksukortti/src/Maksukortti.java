ad3002723fe3850f3f2d1e2186aaaee49fd17058
ti <tvierine@helsinki.fi>
2018-11-07 11:34:42 +0200

    Haettu ja purettu Unicafe.zip viikon 2 repositorioon

--- laskarit/viikko2/Maksukortti/src/Maksukortti.java
+++ laskarit/viikko2/Maksukortti/src/Maksukortti.java
@@ -24,7 +24,7 @@ public class Maksukortti {
     }
  
     public void syoMaukkaasti() {
-        if (this.arvo > MAUKAS) {
+        if (this.arvo >= MAUKAS) {
             this.arvo -= MAUKAS;
         }
     }
