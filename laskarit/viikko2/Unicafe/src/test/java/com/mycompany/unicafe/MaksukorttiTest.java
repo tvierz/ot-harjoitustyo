dba78f88749bf3b5565133d8b34ab2b2ffc40324
ti <tvierine@helsinki.fi>
2018-11-07 12:59:12 +0200

    Unicafe kohdan maksukorttitestit tehty ja tarkistettu 100% kattaviksi jacoco index kautta

--- laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/MaksukorttiTest.java
+++ laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/MaksukorttiTest.java
@@ -17,4 +17,48 @@ public class MaksukorttiTest {
     public void luotuKorttiOlemassa() {
         assertTrue(kortti!=null);      
     }
+    
+    //Test that the card has value it was given on creation
+    @Test
+    public void kortinAlkusaldoOikein(){
+        assertEquals("saldo: 0.10", kortti.toString());
+       
+    }
+    
+    //Test that card can be charged and that the charged value is correct
+    @Test
+    public void kortinLatausToimiiOikein(){
+        kortti.lataaRahaa(100);
+        assertEquals("saldo: 1.10", kortti.toString());
+    }
+    
+    //Test that the card's value can't go into negatives
+    @Test
+    public void saldoVaheneeJosRahaaRiittavasti(){
+        kortti.lataaRahaa(240);
+        kortti.otaRahaa(120);
+        assertEquals("saldo: 1.30", kortti.toString());
+    }
+    
+    //Test that card's value won't change if there isn't enough value on it
+    @Test
+    public void saldoEiMuutuJosEiRahaa(){
+        kortti.otaRahaa(200);
+        assertEquals("saldo: 0.10", kortti.toString());
+    }
+    
+    //returns true if card has enough value, false otherwise
+    @Test
+    public void palauttaaTrueJosRahaRiittiMuutenFalse(){
+        assertTrue(kortti.otaRahaa(5));
+        assertFalse(kortti.otaRahaa(153));
+    }
+    
+    //Test saldo()
+    @Test
+    public void saldoOikein(){
+        assertEquals(kortti.saldo(), 10);
+    }
+    
+
 }
