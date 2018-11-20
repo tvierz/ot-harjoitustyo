bde911d686e5e8768e7b4685282cdad99300f057
ti <tvierine@helsinki.fi>
2018-11-20 10:33:29 +0200

    Created SilkSpinner harjoitustyo project

---
+++ Harjoitustyo/SilkSpinner/src/main/java/com/mycompany/silkspinner/SilkSpinnerMain.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.silkspinner;
+
+/**
+ *
+ * @author tvierine
+ */
+public class SilkSpinnerMain {  
+    public static void main(String[] args) {
+        int a = 92;
+        System.out.println(a);
+    }
+
+      
+}
+    
+
