ca33887b6d8cf320edd5cac65ab78bc199592903
tvierz <44639903+tvierz@users.noreply.github.com>
2018-12-21 15:57:40 +0200

    Update DataSavingTest.java

--- SilkSpinner/src/test/java/DataSavingTest.java
+++ SilkSpinner/src/test/java/DataSavingTest.java
@@ -15,18 +15,21 @@ import java.text.*;
 import silkspinapp.silkspindataobjects.User;
 import silkspinapp.logicandoperations.RegisteredUsersLogic;
 import silkspinapp.logicandoperations.SilkSpinDataSaving;
+
 /**
  *
  * @author tvierine
  */
 public class DataSavingTest {
+
     RegisteredUsersLogic ru;
     User u;
     SilkSpinDataSaving s;
+
     public DataSavingTest() {
-        
+
     }
-    
+
     @Before
     public void setUp() {
         ru = new RegisteredUsersLogic();
@@ -36,21 +39,23 @@ public class DataSavingTest {
         u = ru.getUser("Kalmarimies");         //creates dummy 'loggeduser' for the new account
         s = new SilkSpinDataSaving();
     }
-    
+
     @After
     public void tearDown() {
     }
+
     @Test
-    public void newDataIsWrittenToFileAndThenReadCorrectly(){
+    public void newDataIsWrittenToFileAndThenReadCorrectly() {
         ru.save();                                  // saves the data with 1 user on the app
         ru.register("kille", "dille");              //creates bunch of accounts
         ru.register("nille", "tille");
         ru.initialize();                            //doesn't save the 2 accounts created previously but reads the save instead
         assertEquals(ru.getListMap().size(), 1);    //userlist should only have a single entry because the 2 dummy accounts weren't saved
     }
+
     @Test
-    public void newDataIsWrittenToFileAndThenReadCorrectlyConfirmationToSaveTheExtraAccounts(){
-        
+    public void newDataIsWrittenToFileAndThenReadCorrectlyConfirmationToSaveTheExtraAccounts() {
+
         ru.save();                                  // saves the data with 1 user on the app
         ru.register("kille", "dille");              //creates bunch of accounts
         ru.register("nille", "tille");
@@ -58,15 +63,17 @@ public class DataSavingTest {
         ru.initialize();                            //reads the save 
         assertEquals(ru.getListMap().size(), 3);    //userlist should have 3 entries now, because the 2 extra accounts were saved
     }
+
     @Test
-    public void newUsersDataIsSavedAndReadCorrectly(){
+    public void newUsersDataIsSavedAndReadCorrectly() {
         ru.enterData(u, "2, crikey m8");                                        //this should get saved
         ru.save();
         ru.initialize();
         assertEquals(ru.getUser("Kalmarimies").getdataEntries().size(), 1);     // user should have a single saved data entry that was read
     }
+
     @Test
-    public void newUsersDataIsReadCorrectlyAndIsLegible(){                  //tests that unsaved points don't appear to read data and that the data is read so it's information isn't lost
+    public void newUsersDataIsReadCorrectlyAndIsLegible() {                  //tests that unsaved points don't appear to read data and that the data is read so it's information isn't lost
         ru.enterData(u, "20, Hilperttihööri");      //enters a single data entry to the testuser
         ru.initialize();
         assertEquals(ru.getUser("Kalmarimies").getdataEntries().get(1).toString(), "Date of entry: 21.12.2018 AMOUNT: 2.0€ TYPE OF EXPENSE: crikey m8");     //the user should only have the data entry from previous test
@@ -77,4 +84,3 @@ public class DataSavingTest {
     // @Test
     // public void hello() {}
 }
-
